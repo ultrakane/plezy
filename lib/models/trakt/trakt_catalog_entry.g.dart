12618525141c7dfc23748a8c02514a30ce7f1964
edde746 <86283021+edde746@users.noreply.github.com>
2026-07-10 07:32:02 +0200

    fix(ci): regenerate Trakt catalog model

--- lib/models/trakt/trakt_catalog_entry.g.dart
+++ lib/models/trakt/trakt_catalog_entry.g.dart
@@ -6,11 +6,16 @@ part of 'trakt_catalog_entry.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-TraktCatalogEntry _$TraktCatalogEntryFromJson(Map<String, dynamic> json) => TraktCatalogEntry(
-  rank: (json['rank'] as num?)?.toInt(),
-  listedAt: json['listed_at'] as String?,
-  type: json['type'] as String?,
-  watchers: (json['watchers'] as num?)?.toInt(),
-  movie: json['movie'] == null ? null : TraktCatalogMedia.fromJson(json['movie'] as Map<String, dynamic>),
-  show: json['show'] == null ? null : TraktCatalogMedia.fromJson(json['show'] as Map<String, dynamic>),
-);
+TraktCatalogEntry _$TraktCatalogEntryFromJson(Map<String, dynamic> json) =>
+    TraktCatalogEntry(
+      rank: (json['rank'] as num?)?.toInt(),
+      listedAt: json['listed_at'] as String?,
+      type: json['type'] as String?,
+      watchers: (json['watchers'] as num?)?.toInt(),
+      movie: json['movie'] == null
+          ? null
+          : TraktCatalogMedia.fromJson(json['movie'] as Map<String, dynamic>),
+      show: json['show'] == null
+          ? null
+          : TraktCatalogMedia.fromJson(json['show'] as Map<String, dynamic>),
+    );
